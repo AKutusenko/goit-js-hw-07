6d93b9e72306984dbda695169a873402b9b12da8
Artur Kutusenko <kutusenko@gmail.com>
2021-05-23 14:09:57 +0300

    Task2

---
+++ src/js/task-02.js
@@ -0,0 +1,19 @@
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+const ulCreator = items => {
+    items.forEach(item => {
+        const ingredient = document.createElement('li');
+        ingredient.textContent = item;
+        const ingredientsEl = document.querySelector('#ingredients');
+        ingredientsEl.appendChild(ingredient);
+    });
+}
+
+ulCreator(ingredients);
