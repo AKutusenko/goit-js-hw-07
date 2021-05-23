f25100ac2435d6398097d30822b5e1cffb5fc3db
Artur Kutusenko <kutusenko@gmail.com>
2021-05-23 12:55:29 +0300

    Добавляет решение задачи №1

---
+++ src/js/task-01.js
@@ -0,0 +1,20 @@
+const items = document.querySelectorAll('.item');
+
+const itemsCounter = items => {
+    let counter = 0;
+    items.forEach(item => counter += 1);
+    console.log(`В списке ${counter} категории`);
+}
+
+const itemsChildrenCounter = items => {
+    items.forEach(item => {
+        console.log(`Категория: ${item.firstElementChild.textContent}`);
+        const childrenItems = item.querySelectorAll('li');
+        let counter = 0;
+        childrenItems.forEach(item => counter += 1)
+        console.log(`Количество элементов: ${counter}`);        
+    }); 
+}
+
+itemsCounter(items);
+itemsChildrenCounter(items);
