56d04f1178bd881c7ad3cb9425c5f6088aa02198
Artur Kutusenko <kutusenko@gmail.com>
2021-05-27 22:59:24 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,6 +3,9 @@ input.addEventListener("blur", () => {
   if (input.value.length.toString() === input.dataset.length) {
     input.classList.add("valid");
     input.classList.remove("invalid");
+  } else if (input.value.length === 0) {
+    input.classList.remove("valid");
+    input.classList.remove("invalid");
   } else {
     input.classList.remove("valid");
     input.classList.add("invalid");
