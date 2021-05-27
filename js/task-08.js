1bfa2e5f9d1c0edd26af3a44619590ff0ab3fdbe
Artur Kutusenko <kutusenko@gmail.com>
2021-05-27 23:07:10 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -18,20 +18,27 @@ function onInputChange(e) {
 }
 
 function createBoxes() {
+  const divs = [];
+
+  refs.container.style.padding = "25px";
+  refs.container.style.display = "flex";
+  refs.container.style.alignItems = "center";
+  refs.container.style.flexWrap = "wrap";
+
   for (let i = 0; i < amount; i += 1) {
-    refs.container.style.padding = "25px";
-    refs.container.style.display = "flex";
-    refs.container.style.alignItems = "center";
-    refs.container.style.flexWrap = "wrap";
     const div = document.createElement("div");
+
     div.style.width = `${30 + i * 10}px`;
     div.style.height = `${30 + i * 10}px`;
     div.style.backgroundColor =
       "#" + (Math.random().toString(16) + "000000").substring(2, 8);
     div.style.marginRight = "10px";
     div.style.marginBottom = "10px";
-    refs.container.appendChild(div);
+
+    divs.push(div);
   }
+
+  refs.container.append(...divs);
 }
 
 function destroyBoxes() {
